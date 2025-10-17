043f692fbf9ac1a3a060a1ca12e2cb634b383777
Hebert <hafcandido@sga.pucminas.br>
2025-10-16 21:01:45 -0300

    Configurações do Banco de Dados e da Tabela Veículo

---
+++ MF-DEV-BACKEND-2025/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MF_DEV_BACKEND_2025.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext>options) : base(options) { }
+
+        public DbSet <Veículo> Veículos { get; set; }
+    }
+}
---
+++ MF-DEV-BACKEND-2025/Models/Veículo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MF_DEV_BACKEND_2025.Models
+{
+    [Table ("Veículos")]
+    public class Veículo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a placa!")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano de Fabricação!")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Ano do Modelo!")]
+        public int AnoModelo { get; set; }
+    }
+        
+
+}
