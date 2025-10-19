dc1cf1515d0144f84983834f9bb84489aec0e563
Hebert <hafcandido@sga.pucminas.br>
2025-10-19 19:59:21 -0300

    Botão Editar, Visualizar e Deletar

--- MF-DEV-BACKEND-2025/Controllers/VeículosController.cs
+++ MF-DEV-BACKEND-2025/Controllers/VeículosController.cs
@@ -7,7 +7,7 @@ namespace MF_DEV_BACKEND_2025.Controllers
     public class VeículosController : Controller
     {
         private readonly AppDbContext _context;
-        public VeículosController (AppDbContext context)
+        public VeículosController(AppDbContext context)
         {
             _context = context;
         }
@@ -24,9 +24,9 @@ namespace MF_DEV_BACKEND_2025.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create (Veículo veículo) 
+        public async Task<IActionResult> Create(Veículo veículo)
         {
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
 
                 _context.Veículos.Add(veículo);
@@ -37,6 +37,77 @@ namespace MF_DEV_BACKEND_2025.Controllers
 
             return View(veículo);
         }
-  
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veículos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Veículo veículo)
+        {
+            if (id != veículo.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Veículos.Update(veículo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+        
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veículos.FindAsync(id);
+
+            if(dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veículos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veículos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            _context.Veículos.Remove(dados);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
     }
 }
